95b9ae5f6a3eec27532e05a6d68a8a2c39e29f27
The Eternal Bantuman <31964962+Bantuman@users.noreply.github.com>
2022-08-27 18:46:06 +0200

    Implemented video_texture

--- src/rendering/video_texture.cpp
+++ src/rendering/video_texture.cpp
@@ -3,21 +3,48 @@
 #include <file/file_system.hpp>
 #include <path_utils.hpp>
 #include <rendering/vk_initializers.hpp>
-
+#include <logging.hpp>
 #include <cmath>
 
-// FIXME: Implement
+#define RET_EMPTY(a) \
+if(a){ \
+LOG_ERROR("video_texture", "%s", #a); \
+return {};\
+}
 
-static std::shared_ptr<VideoTexture> load_from_mp4(RenderContext&, const std::vector<char>&, bool,
-	bool);
-static std::shared_ptr<VideoTexture> load_from_webm(RenderContext&, const std::vector<char>&, bool,
-	bool);
+#define CALC_FFMPEG_VERSION(a, b, c) ( a << 16 | b << 8 | c )
+
+double r2d(AVRational r)
+{
+	return r.num == 0 || r.den == 0 ? 0. : (double)r.num / (double)r.den;
+}
+
+double get_real_fps(AVFormatContext * aContext, uint16_t aStream)
+{
+	double eps_zero = 0.000025;
+	double fps = r2d(aContext->streams[aStream]->r_frame_rate);
+
+#if LIBAVFORMAT_BUILD >= CALC_FFMPEG_VERSION(52, 111, 0)
+	if (fps < eps_zero)
+	{
+		fps = r2d(aContext->streams[aStream]->avg_frame_rate);
+	}
+#endif
+
+	if (fps < eps_zero)
+	{
+		fps = 1.0 / r2d(aContext->streams[aStream]->codec->time_base);
+	}
+
+	return fps;
+}
 
 VideoTexture::VideoTexture(std::shared_ptr<Image> image, std::shared_ptr<ImageView> imageView,
 	uint32_t numMipMaps)
 	: m_image(std::move(image))
 	, m_imageView(std::move(imageView))
 	, m_numMipMaps(numMipMaps) {}
+
 
 std::shared_ptr<Image> VideoTexture::get_image() const {
 	return m_image;
@@ -31,46 +58,179 @@ uint32_t VideoTexture::get_num_mip_maps() const {
 	return m_numMipMaps;
 }
 
+VideoTexture::FrameStatus VideoTexture::iterate_frame()
+{
+	int valid = false;
+	int readFrame = 0;
+//	int safeValue = 0;
+	//const int maxSafeVal = 10000;
+
+	while (!valid /* || safeValue > maxSafeVal*/)
+	{
+		//safeValue++;
+		readFrame = av_read_frame(m_videoFormatContext, &m_avPacket);
+		if (readFrame >= 0)
+		{
+			if (m_avPacket.stream_index == m_streamIndex)
+			{
+				m_decodedBytes = avcodec_decode_video2(
+					m_videoCodecContext,
+					m_avVideoFrame,
+					&valid,//&myGotFrame,
+					&m_avPacket);
+			}
+			else if (/*FIXME: Implement audio*/ false)
+			{
+			}
+		}
+		else
+		{
+			valid = true;
+		}
+		av_free_packet(&m_avPacket);
+	}
+
+	/*if (safeValue >= maxSafeVal)
+	{
+		RET_EMPTY("Video error: Could not find a valid video frame!");
+	}*/
+
+	return readFrame < 0 ? FrameStatus::END_OR_ERROR : FrameStatus::ITERATE;
+}
+
+void VideoTexture::write_image(RenderContext& ctx, uint16_t width, uint16_t height)
+{
+	if (m_swsVideoContext)
+	{
+		auto result = sws_scale(
+			m_swsVideoContext,
+			m_avVideoFrame->data,
+			m_avVideoFrame->linesize,
+			0,
+			m_videoCodecContext->height,
+			m_avVideoFrameBGR->data,
+			m_avVideoFrameBGR->linesize);
+
+		if (result > 0)
+		{
+			int* data = static_cast<int*>(m_buffer->map());
+			for (uint32_t x = m_videoCodecContext->height; x < width; ++x)
+			{
+				for (uint32_t y = m_videoCodecContext->width; y < height; ++y)
+				{
+					data[x * width + y] = 0;
+				}
+			}
+
+			int rgbIndex = 0;
+			for (uint32_t x = 0; x < m_videoCodecContext->height; ++x)
+			{
+				for (uint32_t y = 0; y < m_videoCodecContext->width; ++y)
+				{
+					{
+						uint8_t* data = m_avVideoFrameBGR->data[0];
+						data[x * width + y] = int((data[rgbIndex + 3]) << 24 |
+							(data[rgbIndex + 2]) << 16 |
+							(data[rgbIndex + 1]) << 8 |
+							(data[rgbIndex]));
+
+						rgbIndex += 4;
+					}
+				}
+			}
+			m_buffer->unmap();
+		}
+	}
+
+	m_frameCount++;
+}
+
+double VideoTexture::get_fps() const
+{
+	return get_real_fps(m_videoFormatContext, m_streamIndex);
+}
+
+// FIXME: Implement
 float VideoTexture::get_milliseconds() const
 {
 	return 0.0f;
 }
 
+// FIXME: Implement
 void VideoTexture::set_milliseconds(float)
 {
 }
 
 uint32_t VideoTexture::get_frame() const
 {
-	return uint32_t();
+	return m_frameCount;
 }
 
-void VideoTexture::set_frame(uint32_t)
+void VideoTexture::set_frame(int64_t timestamp)
 {
+	av_seek_frame(m_videoFormatContext, m_streamIndex, timestamp, AVSEEK_FLAG_BACKWARD);
 }
+
 
 std::shared_ptr<VideoTexture> VideoTextureLoader::load(RenderContext& ctx, const std::string_view& fileName, bool srgb, bool generateMipmaps) {
 	std::vector<char> data = g_fileSystem->file_read_bytes(fileName);
+	RET_EMPTY(data.empty());
+	
+	std::shared_ptr<VideoTexture> video = std::make_shared<VideoTexture>();
+	AVFormatContext*& videoFormatContext = video->m_videoFormatContext;
 
-	if (data.empty()) {
-		return {};
+	av_register_all();
+	auto result = avformat_open_input(&videoFormatContext, fileName.data(), NULL, NULL);
+	RET_EMPTY(result < 0);
+
+	result = avformat_find_stream_info(videoFormatContext, NULL);
+	RET_EMPTY(result < 0);
+
+	for (uint16_t i = 0; i < videoFormatContext->nb_streams; ++i)
+	{
+		if (videoFormatContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			video->m_streamIndex = i;
+			video->m_videoCodecContext = videoFormatContext->streams[i]->codec;
+			if (video->m_videoCodecContext)
+				video->m_videoCodec = avcodec_find_decoder(video->m_videoCodecContext->codec_id);
+		}
+		else if(/*FIXME: Implement audio*/ false)
+		{
+
+		}
 	}
 
-	auto ext = PathUtils::get_file_extension(fileName);
+	if (AVCodecContext* videoContext = video->m_videoCodecContext; video->m_videoCodec)
+	{
+		result = avcodec_open2(videoContext, video->m_videoCodec, NULL);
+		RET_EMPTY(result < 0);
+		video->m_avVideoFrame = avcodec_alloc_frame();
+		video->m_avVideoFrameBGR = avcodec_alloc_frame();
 
-	if (ext.compare("mp4") == 0) {
-		return load_from_mp4(ctx, data, srgb, generateMipmaps);
+		AVPixelFormat format = AV_PIX_FMT_RGBA;
+		video->m_numberBytes = avpicture_get_size(format, videoContext->width, videoContext->height);
+		video->m_videoBuffer = reinterpret_cast<uint8_t*>(av_malloc(video->m_numberBytes * sizeof(uint8_t)));
+		avpicture_fill(reinterpret_cast<AVPicture*>(video->m_avVideoFrameBGR), video->m_videoBuffer, format, videoContext->width, videoContext->height);
+
+		//m_buffer = ctx.buffer_create(video->m_numberBytes * sizeof(uint8_t), VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_GPU_ONLY);
+		video->m_swsVideoContext = sws_getContext(
+			videoContext->width,
+			videoContext->height,
+			videoContext->pix_fmt,
+			videoContext->width,
+			videoContext->height,
+			format,
+			SWS_BILINEAR,
+			NULL,
+			NULL,
+			NULL
+		);
 	}
-	else {
-		return load_from_webm(ctx, data, srgb, generateMipmaps);
+	else
+	{
+		RET_EMPTY("m_videoCodecContext or m_videoCodec was nullptr");
 	}
-}
-
-static std::shared_ptr<VideoTexture> load_from_webm(RenderContext& /*ctx*/, const std::vector<char>& /*data*/, bool /*srgb*/, bool /*generateMipmaps*/) {
-	return std::shared_ptr<VideoTexture>();
-}
-
-static std::shared_ptr<VideoTexture> load_from_mp4(RenderContext& /*ctx*/, const std::vector<char>& /*data*/, bool /*srgb*/, bool /*generateMipmaps*/) {
-	return std::shared_ptr<VideoTexture>();
+	return video;
 }
 
--- src/rendering/video_texture.hpp
+++ src/rendering/video_texture.hpp
@@ -15,7 +15,15 @@ extern "C"
 #include "ffmpeg-2.0/libswscale/swscale.h"
 }
 
+struct VideoTextureLoader;
+
 class VideoTexture {
+	enum class FrameStatus
+	{
+		ITERATE = 0,
+		END_OR_ERROR = 1
+	};
+	friend class VideoTextureLoader;
 public:
 	explicit VideoTexture(std::shared_ptr<Image> video, std::shared_ptr<ImageView> imageView,
 		uint32_t numMipMaps);
@@ -23,15 +31,40 @@ public:
 	std::shared_ptr<Image> get_image() const;
 	std::shared_ptr<ImageView> get_image_view() const;
 	uint32_t get_num_mip_maps() const;
+
+	FrameStatus iterate_frame();
+	void write_image(RenderContext& ctx, uint16_t width, uint16_t height);
+
+	double get_fps() const;
+
 	float get_milliseconds() const;
 	void set_milliseconds(float milliseconds);
 	uint32_t get_frame() const;
-	void set_frame(uint32_t);
+	void set_frame(int64_t);
 
 private:
+	AVCodec* m_videoCodec;
+
+	AVCodecContext* m_videoCodecContext;
+	AVCodecContext* m_audioCodecContext;
+	SwsContext* m_swsVideoContext;
+	SwrContext* m_swsAudioContext;
+	AVFormatContext* m_videoFormatContext;
+
+	AVFrame* m_avVideoFrame;
+	AVFrame* m_avVideoFrameBGR;
+	AVFrame* m_avAudioFrame;
+	uint8_t* m_videoBuffer;
+	AVPacket m_avPacket;
+
+	std::shared_ptr<Buffer> m_buffer;
 	std::shared_ptr<Image> m_image;
 	std::shared_ptr<ImageView> m_imageView;
 	uint32_t m_numMipMaps;
+	uint32_t m_frameCount;
+	uint32_t m_decodedBytes;
+	uint32_t m_numberBytes;
+	uint16_t m_streamIndex;
 };
 
 struct VideoTextureLoader {
